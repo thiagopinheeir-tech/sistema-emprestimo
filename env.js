f305fd74632ff2e8c16b6209115b4ec56feb978b
Your Name <you@example.com>
2025-08-16 14:22:48 -0300

    Mensagem do commit

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 const SUPABASE_CONFIG = {
-  url: 'https://ilwiovzrdxqoxzaqd.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlsi6ovzdqoxzaqd...',
+  url: 'https://ilwiovzrdxqoxzaqdjtx.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlsd2lvdnpyZHhxb3h6YXFkanR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyOTM1NTcsImV4cCI6MjA3MDg2OTU1N30.MgaEIVNXa9HPZ4K70-vSL7VKPdYR-Gw3ouZlZ0tTM68',
 };
